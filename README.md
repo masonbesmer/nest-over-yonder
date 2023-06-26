343afb491c51ce4e1c45fc1e9cd0309b175cd99f
aaronalexander <aaron.alexander@zendesk.com>
2023-06-26 18:52:23 -0500

    updated my code with Josh's code

---
+++ README.md
@@ -0,0 +1,9 @@
+# nest-over-yonder
+CSCE3444 Project from Agile Minds
+
+To run, first make sure that you have NPM installed. Then navigate to the directory that contains the code (nest-over-yonder) and do:
+- `npm install`
+- `npm run dev`
+
+then open the local server in chrome.
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Listing from './components/Listing'
+import Header from './components/Header'
 import image from "../public/house1/house1.png"
 
 const sampledescription = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta nam mollitia tempore deleniti officiis vero error pariatur in a voluptatibus corporis incidunt, placeat, rem iure cupiditate, dolorum maiores tempora?"
@@ -9,9 +10,8 @@ function App() {
 
   return (
     <>
-      <div className="header">
-        <h1>Header</h1>
-      </div>
+      <Header />
+      
       <Listing src="../public/house1/house1.png" title='Huge House' description={sampledescription} location='Lakewood, Texas' price='$500' rating='4.89'/>
       <Listing src="../public/house1/house2.png" title='Enormous House' description={sampledescription} location='Denton, Texas' price='$500' rating='4.59'/>
       <Listing src="../public/house1/house3.png" title='Humongous House' description={sampledescription} location='Plano, Texas' price='$500' rating='3.12'/>
