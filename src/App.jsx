05da1043cf3d5e7e2fa140b56c4d4bd0e4f5155d
aaronalexander <aaron.alexander@zendesk.com>
2023-06-28 19:05:18 -0500

    fixed map

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
+import { useState, useEffect } from "react";
+import Listing from "./components/Listing";
+import HomePage from "./pages/HomePage";
+import ListingPage from "./pages/ListingPage";
+import Header from "./components/Header";
 
-import { useState, useEffect } from 'react'
-import Listing from './components/Listing'
-import HomePage from './pages/HomePage'
-import ListingPage from './pages/ListingPage'
-import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-
-import image from "../public/house1/house1.png"
-import Map from './components/Map'
-import Filter from './components/Filter'
-
-
-
+import image from "../public/house1/house1.png";
+import Map from "./components/Map";
+import Filter from "./components/Filter";
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   // infoWindow.setPosition(pos);
@@ -22,12 +18,12 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   //     : "Error: Your browser doesn't support geolocation."
   // );
   // infoWindow.open(map);
-  Console.log("uh oh stinky")
+  console.log("uh oh stinky");
 }
 
-function App(props) {
-  const [count, setCount] = useState(0)
-  const [pos, setPos] = useState(null)
+function App() {
+  const [count, setCount] = useState(0);
+  const [pos, setPos] = useState(null);
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -50,21 +46,20 @@ function App(props) {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header/>
-        <Map location={[33.185755896934694, -96.80554467522724]} zoomLevel={15}/>
-    
+        <Header />
+        <Map
+          location={[33.185755896934694, -96.80554467522724]}
+          zoomLevel={15}
+        />
+        TEST
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/listing/:id' element={<ListingPage />} />
-          
+          <Route path="/" element={<HomePage />} />
+          <Route path="/listing/:id" element={<ListingPage />} />
         </Routes>
-        <Filter/>
-
+        <Filter />
       </div>
-      
-      
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
