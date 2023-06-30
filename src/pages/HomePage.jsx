3a7764a4a2fdbdb0f0be3ecf99e6848cc714c74b
Aaron Alexander <aaron.3alexander@gmail.com>
2023-06-30 16:59:30 -0500

    Changed homepage styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,54 @@
 import React from "react";
 import Listing from "../components/Listing";
+import Map from "../components/Map";
 
 const sampledescription =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus soluta nam mollitia tempore deleniti officiis vero error pariatur in a voluptatibus corporis incidunt, placeat, rem iure cupiditate, dolorum maiores tempora?";
 
 function HomePage() {
   return (
-    <div>
-      <Listing
-        id="1"
-        src="../public/house1/house1.png"
-        title="Huge House"
-        description={sampledescription}
-        location="Lakewood, Texas"
-        price="$500"
-        rating="4.89"
-      />
+    <div
+      style={{
+        marginTop: "4rem",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <div className="listings">
+        <Listing
+          id="1"
+          src="../public/house1/house1.png"
+          title="Huge House"
+          description={sampledescription}
+          location="Lakewood, Texas"
+          price="$500"
+          rating="4.89"
+        />
 
-      <Listing
-        id="2"
-        src="../public/house1/house2.png"
-        title="Enormous House"
-        description={sampledescription}
-        location="Denton, Texas"
-        price="$500"
-        rating="4.59"
-      />
+        <Listing
+          id="2"
+          src="../public/house1/house2.png"
+          title="Enormous House"
+          description={sampledescription}
+          location="Denton, Texas"
+          price="$500"
+          rating="4.59"
+        />
 
-      <Listing
-        id="3"
-        src="../public/house1/house3.png"
-        title="Humongous House"
-        description={sampledescription}
-        location="Plano, Texas"
-        price="$500"
-        rating="3.12"
+        <Listing
+          id="3"
+          src="../public/house1/house3.png"
+          title="Humongous House"
+          description={sampledescription}
+          location="Plano, Texas"
+          price="$500"
+          rating="3.12"
+        />
+      </div>
+      <Map
+        location={{ lat: 33.185755896934694, lng: -96.80554467522724 }}
+        //location={pos}
+        zoomLevel={15}
       />
     </div>
   );
