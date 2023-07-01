fba41ed1ddb9bfd437e4a86c9fc2f583853b366d
Josh <joshsterken@gmail.com>
2023-07-01 01:43:30 -0500

    can't get search button to route to SearchPage, plz send help

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../styles.css'
+
+function SearchPage() {
+    return (
+        <>   
+            <div
+            style={{
+                backgroundColor: "lightgrey",
+                marginTop: "4rem",
+                display: "flex",
+                padding: "10px",
+                height: "1000vh",
+            }}>
+                <h1>TEST</h1>
+                <p>These would be the results</p> 
+            </div>
+
+            <img src="../public/logo.png" />      
+        </>
+    );
+}
+
+export default SearchPage;
